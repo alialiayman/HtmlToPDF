10afe9d62dbb4ce2a7a5f68cbd23119023dce306
Ayman Ali <aliayman@alumni.harvard.edu>
2018-05-10 11:41:12 -0700

    Initial Commmit

---
+++ HtmlToPDF/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace HtmlToPDF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.dfsUrl = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dfsCookie = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dfsFileName = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(561, 255);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Save as Pdf";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dfsUrl
+            // 
+            this.dfsUrl.Location = new System.Drawing.Point(77, 13);
+            this.dfsUrl.Name = "dfsUrl";
+            this.dfsUrl.Size = new System.Drawing.Size(464, 20);
+            this.dfsUrl.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "GET";
+            // 
+            // dfsCookie
+            // 
+            this.dfsCookie.Location = new System.Drawing.Point(77, 56);
+            this.dfsCookie.Multiline = true;
+            this.dfsCookie.Name = "dfsCookie";
+            this.dfsCookie.Size = new System.Drawing.Size(464, 193);
+            this.dfsCookie.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Cookie";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 259);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "File Name";
+            // 
+            // dfsFileName
+            // 
+            this.dfsFileName.Location = new System.Drawing.Point(77, 255);
+            this.dfsFileName.Name = "dfsFileName";
+            this.dfsFileName.Size = new System.Drawing.Size(464, 20);
+            this.dfsFileName.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(648, 328);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.dfsFileName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dfsCookie);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dfsUrl);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox dfsUrl;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox dfsCookie;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox dfsFileName;
+    }
+}
+
---
+++ HtmlToPDF/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Codaxy.WkHtmlToPdf;
+
+namespace HtmlToPDF
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(dfsFileName.Text)) return;
+            if (string.IsNullOrEmpty(dfsUrl.Text)) return;
+            var coo = new Dictionary<string, string>();
+            //Cookie: 
+            //f5_cspm = 1234; 
+            //visa.mofa.gov.sa = i5un5pi3nzbzmobfyrtvzqmc; 
+            //__RequestVerificationToken = MVoR0xYlGkXY6_6FVpWS5jYaxr8fWM4sACfe7x8ZjYrdSsjSq08okLL6J2Vbe4NbTlefAFQ4pg4w3ygCv4sC4BtlhiaHe8f57bou89rH_t01; 
+            //BIGipServerVS_Visa_Eexternal.app~VS_Visa_Eexternal_pool = !6wSge + C0lxnxGiLT1TJS3QOVPD1sHPsr9MHFfQKl / +nYxu6ruQrDsYyw2PFRzD3MDn2FvoXkJ8gxEA ==; 
+            //TS0156c15d = 011b14958a4afa8b83e2ea5361a9889b5bbacb920830ed2e0e3cf17e31860aa79b558cc3ded5f824736a2871eb8c978409ed5b878b7d937c9287540f25f2c52eba7310f272d31f0186a55dc2fc7c4229f86f4686851387a7744d268eec9c15d20b348d96f5
+            if (!string.IsNullOrEmpty(dfsCookie.Text))
+            {
+                var items = dfsCookie.Text.Split(';');
+                foreach (var item in items)
+                {
+                    coo.Add(item.Split('=')[0], item.Split('=')[1]);
+                }
+            }
+            PdfConvert.ConvertHtmlToPdf(new PdfDocument
+            {
+                Url = dfsUrl.Text, 
+                Cookies = coo,
+                //HeaderLeft = "[title]",
+                //HeaderRight = "[date] [time]",
+                //FooterCenter = "Page [page] of [topage]"
+
+            }, new PdfOutput
+            {
+                OutputFilePath = dfsFileName.Text
+            });
+        }
+    }
+}
